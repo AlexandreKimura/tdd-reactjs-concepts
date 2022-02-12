455cde46bf4d53024ec97f5b950c80adbcc2296f
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-02-12 00:43:57 -0300

    Pass multiple params

--- src/Hooks/useCounter/useCounter.test.js
+++ src/Hooks/useCounter/useCounter.test.js
@@ -1,16 +1,37 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import useCounter from "./useCounter";
 
-test("Should use counter", () => {
-  const { result } = renderHook(() => useCounter(9000));
+test("Should reset counter to update initial value", () => {
+  let initialValue = 0;
+  const { result, rerender } = renderHook(() => useCounter(initialValue));
+
+  initialValue = 10;
+  rerender();
 
   // expect(result.current.count).toBe(0);
   // expect(typeof result.current.increment).toBe("function");
 
   act(() => {
-    result.current.increment();
+    //result.current.increment();
+    result.current.reset();
   });
 
   // expect(result.current.count).toBe(1);
-  expect(result.current.count).toBe(9001);
+  //expect(result.current.count).toBe(9001);
+  expect(result.current.count).toBe(10);
+});
+
+test("Should reset counter to update initial value with multiple params", () => {
+  const { result, rerender } = renderHook(
+    ({ initialValue }) => useCounter(initialValue),
+    { initialProps: { initialValue: 0 } }
+  );
+
+  rerender({ initialValue: 10 });
+
+  act(() => {
+    result.current.reset();
+  });
+
+  expect(result.current.count).toBe(10);
 });
