98c984dbeee37abad86692946ababd46595da673
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-02-12 01:10:57 -0300

    Custom step to counter

--- src/Hooks/useCounter/useCounter.test.js
+++ src/Hooks/useCounter/useCounter.test.js
@@ -1,37 +1,51 @@
 import { renderHook, act } from "@testing-library/react-hooks";
-import useCounter from "./useCounter";
+import useCounter, { CounterStepProvider } from "./useCounter";
 
-test("Should reset counter to update initial value", () => {
-  let initialValue = 0;
-  const { result, rerender } = renderHook(() => useCounter(initialValue));
+// test("Should reset counter to update initial value", () => {
+//   let initialValue = 0;
+//   const { result, rerender } = renderHook(() => useCounter(initialValue));
 
-  initialValue = 10;
-  rerender();
+//   initialValue = 10;
+//   rerender();
 
-  // expect(result.current.count).toBe(0);
-  // expect(typeof result.current.increment).toBe("function");
+//   // expect(result.current.count).toBe(0);
+//   // expect(typeof result.current.increment).toBe("function");
 
-  act(() => {
-    //result.current.increment();
-    result.current.reset();
-  });
+//   act(() => {
+//     //result.current.increment();
+//     result.current.reset();
+//   });
 
-  // expect(result.current.count).toBe(1);
-  //expect(result.current.count).toBe(9001);
-  expect(result.current.count).toBe(10);
-});
+//   // expect(result.current.count).toBe(1);
+//   //expect(result.current.count).toBe(9001);
+//   expect(result.current.count).toBe(10);
+// });
 
-test("Should reset counter to update initial value with multiple params", () => {
-  const { result, rerender } = renderHook(
-    ({ initialValue }) => useCounter(initialValue),
-    { initialProps: { initialValue: 0 } }
+// test("Should reset counter to update initial value with multiple params", () => {
+//   const { result, rerender } = renderHook(
+//     ({ initialValue }) => useCounter(initialValue),
+//     { initialProps: { initialValue: 0 } }
+//   );
+
+//   rerender({ initialValue: 10 });
+
+//   act(() => {
+//     result.current.reset();
+//   });
+
+//   expect(result.current.count).toBe(10);
+// });
+
+test("Should use custom step when incrementing", () => {
+  const wrapper = ({ children }) => (
+    <CounterStepProvider step={2}>{children}</CounterStepProvider>
   );
 
-  rerender({ initialValue: 10 });
+  const { result } = renderHook(() => useCounter(), { wrapper });
 
   act(() => {
-    result.current.reset();
+    result.current.increment();
   });
 
-  expect(result.current.count).toBe(10);
+  expect(result.current.count).toBe(2);
 });
